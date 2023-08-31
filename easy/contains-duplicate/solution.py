d89b337ea1e145c5eeb91452916d801cadd6887e
Peskov Sergey <peskovdev@proton.me>
2023-08-31 21:08:52 +0600

    refactor

--- easy/contains-duplicate/solution.py
+++ easy/contains-duplicate/solution.py
@@ -1,4 +1,4 @@
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
-        hash_set = set(nums)
-        return len(hash_set) != len(nums)
+        hashset = set(nums)
+        return len(hashset) != len(nums)
