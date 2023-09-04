b1edebb100bb7c03a8edb25af383cf2e45650b53
Peskov Sergey <peskovdev@proton.me>
2023-09-04 17:43:22 +0600

    Refactor generate-parethesis

--- medium/generate-parentheses/stack_list_solution.py
+++ medium/generate-parentheses/stack_list_solution.py
@@ -3,22 +3,23 @@ from copy import deepcopy
 
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
-        self.store: set[str] = set()
+        store: set[str] = set()
         stack: list[list] = [[] for _ in range(n)]
-        self.generate(stack)
-        return self.store  # type: ignore
 
-    def generate(self, stack: list[list]) -> None:
-        for i in range(len(stack) - 1, 0, -1):
-            if len(stack) > 1:
-                new_stack = deepcopy(stack[:i]) + deepcopy(stack[i + 1:])
-                new_stack[i - 1].append(stack[i])
-                self.generate(new_stack)
-        combination = (
-            str(stack)
-            .replace(" ", "")
-            .replace(",", "")
-            .replace("[", "(")
-            .replace("]", ")")
-        )[1:-1]
-        self.store.add(combination)
+        def backtrack(stack: list[list]) -> None:
+            for i in range(len(stack) - 1, 0, -1):
+                if len(stack) > 1:
+                    new_stack = deepcopy(stack[:i]) + deepcopy(stack[i + 1:])
+                    new_stack[i - 1].append(stack[i])
+                    backtrack(new_stack)
+            combination = (
+                str(stack)
+                .replace(" ", "")
+                .replace(",", "")
+                .replace("[", "(")
+                .replace("]", ")")
+            )[1:-1]
+            store.add(combination)
+
+        backtrack(stack)
+        return store  # type: ignore
