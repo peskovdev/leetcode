5646a8f5c5ed7a8faf72dacff1ee84ddfff1cd53
Peskov Sergey <peskovdev@proton.me>
2023-09-06 18:14:55 +0600

    Add subtree-of-another-tree

---
+++ easy/subtree-of-another-tree/solution.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSubtree(
+        self, root: Optional[TreeNode], subRoot: Optional[TreeNode]
+    ) -> bool:
+        if subRoot is None: return True
+        if root is None: return False
+
+        if self.is_same_tree(root, subRoot):
+            return True
+
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def is_same_tree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is q is None:
+            return True
+        if p is None or q is None or q.val != p.val:
+            return False
+        return self.is_same_tree(p.left, q.left) and self.is_same_tree(p.right, q.right)
