8842ae8138b56e3b5c14c1a55cbb02b9eddfc5de
Peskov Sergey <peskovdev@proton.me>
2023-10-01 15:44:36 +0600

    Add implement-queue-using-stacks

--- easy/implement-queue-using-stacks/solution.py
+++ easy/implement-queue-using-stacks/solution.py
@@ -4,21 +4,23 @@ class MyQueue:
         self.popstack = []
 
     def push(self, x: int) -> None:
-        while self.popstack:
-            self.stack.append(self.popstack.pop())
         self.stack.append(x)
 
     def pop(self) -> int:
+        if self.popstack:
+            return self.popstack.pop()
+
         while self.stack:
             self.popstack.append(self.stack.pop())
         return self.popstack.pop()
-        # return self.popstack.pop() if self.popstack else -1
 
     def peek(self) -> int:
+        if self.popstack:
+            return self.popstack[-1]
+
         while self.stack:
             self.popstack.append(self.stack.pop())
         return self.popstack[-1]
-        # return self.popstack[-1] if self.popstack else -1
 
     def empty(self) -> bool:
         return not (self.stack or self.popstack)
