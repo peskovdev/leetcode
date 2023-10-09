d4ac57490726436982046c310db64f44ba232ab8
Peskov Sergey <peskovdev@proton.me>
2023-10-09 11:56:17 +0600

    Add jewels-and-stones

---
+++ easy/jewels-and-stones/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewels = set(jewels)
+
+        res = 0
+        for s in stones:
+            if s in jewels:
+                res += 1
+
+        return res
