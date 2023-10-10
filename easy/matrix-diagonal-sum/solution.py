5374e7b66b54ff24f061baa35a55f8592035b1ec
Peskov Sergey <peskovdev@proton.me>
2023-10-10 16:54:30 +0600

    Add matrix-diagonal-sum

--- easy/matrix-diagonal-sum/solution.py
+++ easy/matrix-diagonal-sum/solution.py
@@ -7,3 +7,14 @@ class Solution:
                 res += mat[n - i][i]
 
         return res
+
+
+# Or with substraction
+# class Solution:
+#     def diagonalSum(self, mat: list[list[int]]) -> int:
+#         res, n = 0, len(mat)
+#         for i in range(len(mat)):
+#             res += mat[i][i]
+#             res += mat[n - 1 - i][i]
+#
+#         return res - (mat[n // 2][n // 2] if n % 2 else 0)
