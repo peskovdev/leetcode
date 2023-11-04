dd12ca2acba58e0ab55ffb2eb91e0ff6818c1968
Peskov Sergey <peskovdev@proton.me>
2023-11-05 02:18:41 +0600

    Add Go valid-anagram

---
+++ easy/valid-anagram/solution.go
@@ -0,0 +1,17 @@
+package main
+
+func isAnagram(s string, t string) bool {
+    count := make(map[rune]int)
+    for _, r := range s {
+      count[r]++
+    }
+    for _, r := range t {
+      count[r]--
+    }
+    for _, freq := range count {
+      if freq != 0 {
+        return false
+      }
+    }
+    return true
+}
