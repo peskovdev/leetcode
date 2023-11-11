b14dfb82c6c67d19d793cc1f50864a9637980dcf
Peskov Sergey <peskovdev@proton.me>
2023-11-11 18:25:26 +0600

    Add Go reverse-linked-list

---
+++ easy/reverse-linked-list/solution.go
@@ -0,0 +1,18 @@
+package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	for head != nil {
+		nxt := head.Next
+		head.Next = prev
+		prev = head
+		head = nxt
+	}
+	return prev
+}
